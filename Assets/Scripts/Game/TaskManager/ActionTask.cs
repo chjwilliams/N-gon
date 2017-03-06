c3a6db8c764a248d052f7cf7502c593374c7d8a4
chjwilliams <chrsjwilliams@gmail.com>
2017-03-06 12:20:11 -0500

    Finished Implementation of Task Manager

---
+++ Assets/Scripts/Game/TaskManager/ActionTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleTask;
+
+public class ActionTask : Task 
+{
+
+	private readonly Action _action;
+
+	public ActionTask(Action action)
+	{
+		_action = action;
+	}
+	
+	protected override void Init()
+	{
+		SetStatus(TaskStatus.Success);
+		_action();
+	}
+}
